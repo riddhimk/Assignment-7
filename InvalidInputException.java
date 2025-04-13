00bb6b555c127da780b14a07b2b55c11db5c42ce
Riddhim Kawdia <riddhim10000@gmail.com>
2025-04-13 14:04:55 +0530

    Created InvalidInputException.java

---
+++ InvalidInputException.java
@@ -0,0 +1,10 @@
+// InvalidInputException.java
+// Custom exception to handle invalid inputs from the user
+
+public class InvalidInputException extends Exception
+{
+    public InvalidInputException(String message)
+    {
+        super(message);
+    }
+}

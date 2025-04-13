81b5333069e964c76d70d7edaa897f2b4b0a61c8
Riddhim Kawdia <riddhim10000@gmail.com>
2025-04-13 14:04:00 +0530

    Method to take user input

--- Input.java
+++ Input.java
@@ -5,3 +5,8 @@ import java.util.InputMismatchException;
 
 public class Input
 {
+    // method to take input from user
+    int[] input() throws InvalidInputException
+    {
+        Scanner scan = new Scanner(System.in);
+

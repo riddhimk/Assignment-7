c9dfa9ce2f50de736632859c6412ccfc8fd6254a
Riddhim Kawdia <riddhim10000@gmail.com>
2025-04-13 14:07:26 +0530

    Created Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+/*
+Name  - Riddhim Kawdia
+PRN   - 23070126106
+Batch - AIML B2
+*/
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+
+        // Infinite loop for calculator until user exits

179d48b85deaf919a5539e6c10a97c37219fdaf1
Riddhim Kawdia <riddhim10000@gmail.com>
2025-04-13 14:04:28 +0530

    try-catch block to handle invalid input exception

--- Input.java
+++ Input.java
@@ -10,3 +10,24 @@ public class Input
     {
         Scanner scan = new Scanner(System.in);
 
+        try
+        {
+            // input numbers
+            System.out.print("Enter num1: ");
+            int num1 = scan.nextInt();
+            System.out.print("Enter num2: ");
+            int num2 = scan.nextInt();
+
+            // create and return an array to store the numbers
+            int[] nums = new int[2];
+            nums[0] = num1;
+            nums[1] = num2;
+
+            return nums;
+        }
+        catch (InputMismatchException e)
+        {
+            throw new InvalidInputException("Invalid input! Enter valid number");
+        }
+    }
+}
